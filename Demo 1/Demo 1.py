b422715513fb0427e4b3931936ba16f7c9d37dde
panthativishnu6248 <panthativishnu6248@gmail.com>
2025-11-11 14:30:02 +0530

    Delete Demo 1 directory

--- Demo 1/Demo 1.py
+++
@@ -1,8 +0,0 @@
-import sys
-from awsglue.utils import getResolvedOptions
-
-# Define the parameter you expect
-args = getResolvedOptions(sys.argv, ['Test_param'])
-
-# Print the parameter value
-print("The value of Test_param is:", args['Test_param'])
